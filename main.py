09f7d27132e2562c089e4fbc8001a0e9de6f57f6
akxaalex16 <akxaalex16@gmail.com>
2024-06-18 19:12:10 -0700

    update main.py
    print and input statements/functions

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+print("What is your fav color? ")
+color = input()
+print(color + " is my fav color.")
+
+name = input("What is your name? ")
+occupation = input("What is your occupation? ")
+num_of_years = input("How long have you been in this occupation? ")
